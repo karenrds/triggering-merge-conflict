f09e1d17349e36ea9af5e47a5e4f8f460445cc52
revathisekar35 <revathisekar2010@gmail.com>
2022-02-03 10:17:36 +0000

    Added branch messages

--- src/java/com/techreturners/TestMain.java
+++ src/java/com/techreturners/TestMain.java
@@ -3,6 +3,8 @@ public class TestMain {
 	public void static main(String args[]) {
 		System.out.println("Entering main method");
 		System.out.println("changed message");
+		System.out.println("We are in main branch");
+		System.out.println("Lets start work");
 	}
 
 }
